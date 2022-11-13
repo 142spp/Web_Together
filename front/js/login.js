037758e1d5104afc3227cae5b29dcaf7275a4eed
142spp <ehdrms3835@gmail.com>
2022-11-13 15:31:16 +0900

    login

--- front/js/login.js
+++ front/js/login.js
@@ -1,9 +1,25 @@
 function login_submit(){
     const id = $('#account_id')[0].value;
     const password = $('#account_password')[0].value;
-    if(id=="") message = "아이디를 입력하세요";
-    else if(password=="") message = "패스워드를 입력하세요";
-    else message = "성공적입니다.";
-    console.log(id);
-    alert(message);
+    if(id=="" || password=="") {
+        message = "입력하지 않은 부분이 있습니다."
+        alert(message);
+    }
+    goToLogin = e => {
+        e.preventDefault();
+        fetch('http://', {
+            method : 'POST',
+            headers : {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                account_id: id,
+                account_password: password,
+            }),
+        })
+        .then(response => response.json())
+        .then(result => {
+            if (result.message === 'SUCCESS'){
+                alert('Success');
+            }
+        });
+    };
 }
