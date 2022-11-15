a6c409bdd03a2dab614df13112e0d60bad8e68fc
142spp <ehdrms3835@gmail.com>
2022-11-15 15:03:02 +0900

    js update

--- front/js/login.js
+++ front/js/login.js
@@ -1,25 +1,26 @@
-function login_submit(){
+
+function login_submit() {
     const id = $('#account_id')[0].value;
     const password = $('#account_password')[0].value;
-    if(id=="" || password=="") {
+    if (id == "" || password == "") {
         message = "입력하지 않은 부분이 있습니다."
         alert(message);
     }
-    goToLogin = e => {
-        e.preventDefault();
-        fetch('http://', {
-            method : 'POST',
-            headers : {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                account_id: id,
-                account_password: password,
-            }),
-        })
-        .then(response => response.json())
-        .then(result => {
-            if (result.message === 'SUCCESS'){
-                alert('Success');
-            }
-        });
-    };
+    fetch('http://', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            account_id: id,
+            account_password: password,
+        }),
+    })
+    .then(response => response.json())
+    .then(result => {
+        if (result.message === 'SUCCESS') {
+            alert('Success');
+        }else {
+            alert('Failure');
+        }
+    });
+
 }
